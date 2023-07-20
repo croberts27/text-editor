4434bcaa13c657effb058ef2868b12dea19048ed
Calvin Roberts <you@example.com>
2023-07-19 18:30:18 -0600

    updates to database.js

--- Develop/client/src/js/install.js
+++ Develop/client/src/js/install.js
@@ -20,11 +20,8 @@ butInstall.addEventListener("click", async () => {
   if (!promptEvent) {
     return;
   }
-
-  // Show prompt
   promptEvent.prompt();
 
-  // Reset the deferred prompt variable, it can only be used once.
   window.deferredPrompt = null;
 
   butInstall.classList.toggle("hidden", true);
